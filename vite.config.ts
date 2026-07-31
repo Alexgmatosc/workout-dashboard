f061305778109726c6a4288473e404525c7b95d6
Alexander Matos <alexgmatosc@gmail.com>
2026-07-31 11:40:04 +0200

    chore: ajuste de chunkSizeWarningLimit en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, 'src'),
       },
     },
+    build: {
+      chunkSizeWarningLimit: 1000,
+    },
     server: {
       proxy: {
         '/hevy-api': {
